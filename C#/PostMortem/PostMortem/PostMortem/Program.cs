497d7a1228e786134e7d6ef5a7cb4f463b3869ea
Ian Nastajus <ian.nastajus@gmail.com>
2014-11-22 12:32:58 -0500

    const keyword usage ( C#, Visual Studio, Console )
    const must be assigned immediately, there's no delayed assignment

---
+++ C#/PostMortem/PostMortem/PostMortem/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PostMortem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SyntaxConst foo = new SyntaxConst();
+            Console.ReadLine();
+        }
+    }
+
+    class SyntaxConst
+    {
+        const int a = 10;
+        
+        //const int b;
+        //b = 10;
+        //not allowed
+        
+    }
+}
