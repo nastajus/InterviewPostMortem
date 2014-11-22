2d2391a6dfc02552018c2d902f79366c6ad2a0a8
Ian Nastajus <ian.nastajus@gmail.com>
2014-11-22 12:51:47 -0500

    static final keywords used. test #2 (Java)

--- Java/PostMortem/src/com/nastajus/SyntaxFinal.java
+++ Java/PostMortem/src/com/nastajus/SyntaxFinal.java
@@ -4,7 +4,13 @@ package com.nastajus;
  * Created by IAN on 22/11/14.
  */
 public class SyntaxFinal {
+
+    //test #2
+    //static final int a;     //"variable a might not have been initialized" == must initialize here.
+    static final int a = 10;
+
     void bar() {
+        //test #1
         final int a;
         a = 10;
         //a = 11;   //not allowed again
---
+++ Java/PostMortem/src/com/nastajus/SyntaxFinalBuddy.java
@@ -0,0 +1,10 @@
+package com.nastajus;
+
+/**
+ * Created by IAN on 22/11/14.
+ */
+public class SyntaxFinalBuddy {
+    void bar() {
+        int a = 10;
+    }
+}
