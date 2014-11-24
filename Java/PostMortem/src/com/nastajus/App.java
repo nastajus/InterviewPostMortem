be4e681356a5346d2c1108af3ae96c2947929ffd
Ian Nastajus <ian.nastajus@gmail.com>
2014-11-24 01:48:38 -0500

    HashMap<String, String>, .put, putIfAbsent
    hash map isn't iterable :/

--- Java/PostMortem/src/com/nastajus/App.java
+++ Java/PostMortem/src/com/nastajus/App.java
@@ -1,26 +1,41 @@
 package com.nastajus;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.util.HashMap;
 
-/**
- * Created by IAN on 24/11/14.
- */
 public abstract class App {
+	
+	static HashMap<String, String> map = new HashMap<String, String>();
+	
+	static void read() throws IOException{
+		FileReader fr = new FileReader("blooms-verbs.txt");
+		BufferedReader br = new BufferedReader(fr);
+		
+		String line;
+		String[] parts;
+		while ( (line = br.readLine()) != null ){
+			//System.out.println(line);
+			parts = (line.split(","));
+			
+			int count = 0;
+			for ( String part : parts ){
+				count++;
+				part = part.trim();
+				System.out.println(part);
+				if (!map.containsKey(part)) map.put(part, null);
+				else if (count % 2 == 0){
+					//should exist already, based on assumption file order isn't changing.
+					map.putIfAbsent(parts[0], parts[1]);
+				}
+			}
+		}
+		
+//		for (KeyValue pair : map ){
+//			
+//		}
+		
+	}
 
-    static void read() throws IOException {
-
-        FileReader testFileReader = new FileReader("blooms-verbs.txt");
-        BufferedReader bf = new BufferedReader(testFileReader);
-        String line;
-
-        while((line = bf.readLine())!=null){
-            bf.read();
-            System.out.println(line);
-        }
-
-    }
 }
