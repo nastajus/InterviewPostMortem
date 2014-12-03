c86f6142675b0ea71b5598b3507b08f5d13b8bc0
Ian Nastajus <ian.nastajus@gmail.com>
2014-12-03 18:59:26 -0500

    Map to Set conversion? (Java)
    not sure if works, busy now :anguished:

--- Java/PostMortem/src/com/nastajus/App.java
+++ Java/PostMortem/src/com/nastajus/App.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 
 public abstract class App {
 	
@@ -26,20 +27,36 @@ public abstract class App {
 				count++;
 				part = part.trim();
 				System.out.println(part);
-				if (!map.containsKey(part)) map.put(part, null);
+
+				if (!map.containsKey(part)) {
+                    map.put(part, "yo");
+                    if (map.get(part) != null){
+                        System.out.println("CHECKING IF PART IS INSIDE: " + map.get(part));
+                    }
+                    else
+                        System.out.println("WAS NULL");
+                }
+
 				else if (count % 2 == 0){
 					//should exist already, based on assumption file order isn't changing.
 					map.putIfAbsent(parts[0], parts[1]);
 				}
 			}
 		}
-		
+
 		Iterator it = map.entrySet().iterator();
 		while (it.hasNext()){
 			Map.Entry pairs = (Map.Entry)it.next();
 			System.out.println(pairs.getKey() + " = " + pairs.getValue());
 			it.remove(); // avoid problems
 		}
+
+        Set set = map.entrySet();
+        String target = "apply";
+        //if ( set.contains(target) ){      // failed. didn't work
+        if ( map.containsKey(target) ){
+                System.out.println("target " + target + " found.");
+        }
 	}
 
 }
