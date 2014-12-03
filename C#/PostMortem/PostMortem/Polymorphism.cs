8036cc1eb78f410cb7c32798ae0dae94bf79fb97
Ian Nastajus <ian.nastajus@gmail.com>
2014-12-03 18:55:28 -0500

    Polymorphism (C#) started, not done

---
+++ C#/PostMortem/PostMortem/Polymorphism.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostMortem
+{
+    //referencing C# in a Nutshell book
+
+    static class Polymorphism
+    {
+        public void run()
+        {
+            Stock msft = new Stock();
+            House mansion = new House();
+
+            //polymorphism:
+            Display()
+        }
+    }
+
+    abstract class Asset
+    {
+        public string Name;
+
+        public static void Display(Asset asset)
+        {
+            Console.WriteLine(asset.Name);
+        }
+    }
+
+    class Stock : Asset
+    {
+        public Stock() { Name = "Stock"; }
+    }
+
+    class House : Asset
+    {
+        public House() { Name = "House"; }
+    }
+}
